03fc56ee873d4973e4aded200d1cb21fe46cf068
NathanPaynter <nathanelpaynter@gmail.com>
2017-03-06 09:54:04 -0700

    Adjusted script to hide other sections when opening a different section

--- scripts/custom.js
+++ scripts/custom.js
@@ -4,8 +4,15 @@ $(document).ready(function() {
   //ACCORDION
   $('.accQuestion').click(function() {
     $parent_box = $(this).parent();
+
+    // Toggle accordion show/hide
     $parent_box.find('.accAnswer').toggleClass('activeAccAnswer');
     $(this).toggleClass('flipButton');
+
+    // Hide other accordions
+    $('.accQuestion').not($(this)).removeClass('flipButton');
+    $('.accQuestion').not($(this)).next().removeClass('activeAccAnswer');
+
   });
   
 });
